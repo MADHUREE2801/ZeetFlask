759aa91bf78fba886d309d9fe0ced6bbd2a333d9
GOPAVARAM SAI MADHUREE <82037879+MADHUREE2801@users.noreply.github.com>
2023-03-07 13:41:51 +0530

    Add files via upload

---
+++ routes/pages.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const router=express.Router();
+const userController=require('../../../../controllers/users');
+
+router.get("/login",(req,res)=>{
+    // res.send("<h1>Hello Sai hi</h1>");
+    res.render('login');
+ });
+ 
+ router.get("/signup",(req,res)=>{
+     // res.send("<h1>Hello Sai hi</h1>");
+     res.render('signup');
+  });
+  router.get("/",(req,res)=>{
+     res.render('home');
+  });
+  router.get("/welcome",(req,res)=>{
+     res.render('welcome');
+  });
+ 
+ module.exports=router;
